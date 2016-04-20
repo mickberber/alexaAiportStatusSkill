d54c6b1adc463ed3b934f53ad8b105b766b578bf
Michael Berber <mickberber@icloud.com>
2016-04-19 19:51:41 -0700

    [test]: add response tests and function

--- FAADataHelper.js
+++ FAADataHelper.js
@@ -25,6 +25,29 @@ FAADataHelper.prototype.getAirportStatus = function(airportCode) {
 		json: true
 	}
 	return rp(options);
-}
+};
+
+FAADataHelper.prototype.formatAirportStatus = function(airportStatus) {
+	var weather = _.template('The current weather conditions are ${weather}, ${temp} and wind ${wind}.')({
+		weather: airportStatus.weather.weather,
+		temp: airportStatus.weather.temp,
+		wind: airportStatus.weather.wind
+	});
+	if(airportStatus.delay === 'true') {
+		var template = _.template('There is currently a delay for ${airport}. ' + 'The average delay time is ${delayTime}. ' + 'Delay is because of the following: ${delayReason}. ${weather}')
+		return template({
+			airport: airportStatus.name,
+			delayTime: airportStatus.status.avgDelay,
+			delayReason: airportStatus.status.reason,
+			weather: weather
+		});
+	} else {
+		// no delay
+		return _.template('There is currently no delay at ${airport}. ${weather}')({
+			airport: airtportStatus.name,
+			weather: weather
+		});
+	}
+};
 
 module.exports = FAADataHelper;
